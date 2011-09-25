0d8b427adcae1c2e94d51c415d63f547e02f8f7f
Joachim Kainz <jfk@jolira.com>
2011-09-25 01:10:42 +0000

    reformatting code

--- tests/master.js
+++ tests/master.js
@@ -1,11 +1,9 @@
 var util = require("util");
 var backgrounder = require("../lib/backgrounder");
 var worker = backgrounder.spawn(__dirname + "/worker.js");
-
 worker.on("message", function(message) {
     console.log(util.inspect(message, false, 100));
 });
-
 worker.send({
     "title": "hello world!",
     "flag": true
