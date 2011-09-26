d5198822b5f28d7244db53ceba8494c50d095b91
Joachim Kainz <jfk@jolira.com>
2011-09-26 13:43:58 -0700

    adding support for a config message.

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ Yet another wrapper around the [``child_process.spawn``](http://nodejs.org/docs/
 There are a lot of other, similar libraries for Node.js out there, that do similar things ([as this is a very common problem](http://stackoverflow.com/questions/3809165/long-running-computations-in-node-js)). This particular one fits my problem set, as it
 
 * Allows for multiple backrounds works to be created.
-* Uses "require" to manage the code in the sub-modules.
 * Allows users to implement code that is compatible with the [``child_process.fork``](http://nodejs.org/docs/v0.5.6/api/all.html#child_process.fork) call available with v0.5.*.
 * Child processes can use ``console.log`` and other console function; the logs are printed by the main process.
 
@@ -32,7 +31,7 @@ This could be the code of the client:
 ```
 var util = require("util");
 
-console.log('Started the working');
+console.log('Started the worker');
 
 process.on('message', function(message) {
     console.log('Worker received: %s', util.inspect(message, false, 100));
--- examples/master.js
+++ examples/master.js
@@ -1,11 +1,41 @@
 var util = require("util");
 var backgrounder = require("../lib/backgrounder");
+//
+// Spawn the worker in a backround proccess
+//
 var worker = backgrounder.spawn(__dirname + "/worker.js");
+//
+// For this demo, let's just print any message we are receiving from the worker
+//
 worker.on("message", function(message) {
     console.log(util.inspect(message, false, 100));
 });
+//
+// Process messages that indicate that the workder has become idle. Both worker.config as well as
+// worker.send messages result in "idle" messages from the worker when the processing of the messages
+// is complete.
+//
+var counter = 0;
+worker.on("idle", function(message) {
+    if (0 === counter ++) {
+        console.log("configuration completed");
+        return;
+    }
+
+    worker.terminate();
+});
+//
+// This is optional, but some workers need to be configured. This call sends a message, which triggers
+// a "config" event in the client.
+//
+worker.config({
+    "primaryDirective": "don't interfere",
+    "overdrive": true
+});
+//
+// Send a message to the
+//
 worker.send({
     "title": "hello world!",
     "flag": true
 });
-worker.terminate();
