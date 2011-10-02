acbd9e3844841d4ea8c2123c2619385e10da8184
Joachim Kainz <jfk@jolira.com>
2011-10-02 09:57:40 -0700

    now support legacy & callback interface.

--- examples/simple-master.js
+++ examples/simple-master.js
@@ -1,11 +1,9 @@
 var backgrounder = require("../lib/backgrounder");
-
-backgrounder.spawn(__dirname + "/simple-worker.js", function(worker){
-    worker.send({
-        "company": "jolira"
-    }, function(arg1, arg2, arg3) {
-        console.log("Master: client called the callback with %s arguments:",
-            arguments.length, arg1, arg2, arg3);
-        worker.terminate();
-    });
+var worker = backgrounder.spawn(__dirname + "/simple-worker.js");
+worker.send({
+    "company": "jolira"
+}, function(arg1, arg2, arg3) {
+    console.log("Master: client called the callback with %s arguments:",
+        arguments.length, arg1, arg2, arg3);
+    worker.terminate();
 });
