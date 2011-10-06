b8da1273170af70514362fa09a9685ee0e2036c7
Joachim Kainz <jfk@jolira.com>
2011-10-06 14:28:17 -0700

    using a more round-robin approach to scheduling.

--- examples/multi-master.js
+++ examples/multi-master.js
@@ -1,16 +1,17 @@
 var backgrounder = require("../lib/backgrounder");
 var worker = backgrounder.spawn(__dirname + "/multi-worker.js", {
     "children-count" : 5
-});
+}, function(){
+    console.log("Master: Config finished,");
+    var counter = 0;
 
-var counter = 0;
+    for(var idx=0; idx<100; idx++) {
+        worker.send({}, function(arg1, arg2, arg3, arg4, arg5) {
+            console.log("Master: client called the callback with %s arguments:",
+                arguments.length, arg1, arg2, arg3, arg4, arg5);
+            if (++counter === 100) {
+                worker.terminate();
+            }
+        });
+}});
 
-for(var idx=0; idx<10; idx++) {
-    worker.send({}, function(arg1, arg2, arg3) {
-        console.log("Master: client called the callback with %s arguments:",
-            arguments.length, arg1, arg2, arg3);
-        if (++counter === 10) {
-            worker.terminate();
-        }
-    });
-}
--- examples/multi-worker.js
+++ examples/multi-worker.js
@@ -1,5 +1,14 @@
 var http = require('http');
 var id = process.pid;
+var loaded = 0;
+
+/**
+ * There really is no configuration here, but it is essential that we are calling the callback functtion.
+ * Failing to call the callback function will prevent the master from confinuing
+ */
+process.on('config', function(message, callback) {
+    callback();
+});
 
 process.on('message', function(message, callback) {
     var options = {
@@ -9,10 +18,14 @@ process.on('message', function(message, callback) {
     };
 
     http.get(options, function(res) {
-        callback(id, "status-code:", res.statusCode);
+        callback(id, "status-code:", res.statusCode, "loaded", ++loaded);
     }).on('error', function(e) {
         callback("error:", e);
     });
+});
+
+process.on('terminate', function(message, callback) {
+    console.log("Worker: %s Loaded", id, ++loaded);
 });
 
 console.log('Worker: Started %s!', id);
